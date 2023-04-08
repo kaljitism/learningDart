e7aa3626ff4177ceb741de962d74c7d6e5d18004
kaljitism <dwikaljit@gmail.com>
2023-04-08 23:22:06 +0530

    Updates on Operators

--- 02_Operators.dart
+++ 02_Operators.dart
@@ -1,10 +1,12 @@
+import 'dart:ffi';
+
 void main() {
   
   // Defining Operands 
   int a = 13;
   int b = 16;
 
-  //  Operations using Operators 
+  //  Arithmetic Operators 
   print('a + b = ${a + b}'); 
   print('a - b = {a - b}');
   print('a * b = ${a * b}');
@@ -23,11 +25,107 @@ void main() {
   print('prefix decrement op - performs b - 1, returns b - 1 = ${--b}');     
   print('a = $b');
 
-  // print(a == b);
-  // print(a > b);
-  // print(a < b);
-  // print(a <= b);
-  // print(a >= b);
+  // Equality and Relational Operators 
+  print('a == b = ${a == b}');
+  print('a != b = ${a != b}');
+  print('a > b = ${a > b}');
+  print('a < b = ${a < b}');
+  print('a <= b = ${a <= b}');
+  print('a >= b = ${a >= b}\n');
+
+  // Type Test Operators 
+  Map data = {
+    'list': ['object1', 'object2', 'object3']
+  };
+  List list = data['list'] as List;
+  print(list);
+
+  print('$a is Float = ${a is Float}');
+  print('$a is! Float = ${a is! Float}');
+
+  // Assignment Operator 
+  a = 20;
+  print('a = $a');
+
+  // a ??= value, Assign value to a if a is null; otherwise, a stays the same
+  var random_object;
+  random_object  ??= 2;
+  print('Random Object is $random_object');
+
+  random_object ??= 30;
+  print("Random Object is still $random_object as it was not null.\n");
+
+  // Compund Assignment Operator 
+  // a op= b means a = a op b
+
+  // Logical Operators 
+  if (!(a is int)) {
+    print('A is integer');
+  } else {
+    print('! reverse the boolean value');
+  }
+
+  // Logical and &&, or ||
+  print('a = $a');
+  if (a < 50 && a < 10) {
+    print('true 1');
+  } if (a < 50 || a < 10) {
+    print('true 2\n');
+  }
+
+  // Conditional Expressions 
+  (a is int) ? print("option1") : print("option2");
+  (a is String) ? print("option1\n") : print("option2\n");
+
+  b = 5;
+  print("a ?? a = ${a ?? b} ... returning a as a is not null");
+  var rand;
+  print("rand ?? b = ${rand ?? b} ... returning b as rand is null\n");
+
+  // Cascading Notation 
+  // Allows you to make sequence of operations on same object. 
+  
+  // Original Code =>
+    // var paint = Paint();
+    // paint.color = Colors.black;
+    // paint.strokeCap = StrokeCap.round;
+    // paint.strokeWidth = 5.0
+
+  // Cascaded Code =>
+    // var paint = Paint()
+    // ..color = Colors.black
+    // ..strokeCap = StrokeCap.round
+    // ..strokeWidth = 5.0;
+
+  // Using ?.. ensures that ops are not performed on null objects. 
+  
+  // Original Code => 
+    // var button = querySelector('#confirm');
+    // button?.text = 'Confirm';
+    // button?.classes.add('important');
+    // button?.onClick.listen((e) => window.alert('Confirmed!'));
+    // button?.scrollIntoView();
+
+  // Code using ?..
+    // querySelector('#confirm') // Get an object.
+    // ?..text = 'Confirm' // Use its members.
+    // ..classes.add('important')
+    // ..onClick.listen((e) => window.alert('Confirmed!'))
+    // ..scrollIntoView();
+
+  // Nested Cascading =>
+    // final addressBook = (AddressBookBuilder()
+    //       ..name = 'jenny'
+    //       ..email = 'jenny@example.com'
+    //       ..phone = (PhoneNumberBuilder()
+    //             ..number = '415-555-0100'
+    //             ..label = 'home')
+    //           .build())
+    //     .build();
+
+  // Dont use ?.. on a function/ method that returns an object 
+
+
 
 
 
