cdc781317b06efd939d79c6904ed4d6686d28e06
kaljitism <dwikaljit@gmail.com>
2023-04-10 03:23:57 +0530

    Updated Functions

--- 07_functions.dart
+++ 07_functions.dart
@@ -13,13 +13,27 @@ void testIQs({List<int>? iqList}) {
   }
 }
 
+// A function written to demonstrate an example later on
+void multiply2(num num) {
+  print(num * 2);
+}
+
+// Top Level main() function 
 void main() {
   
   testIQs(iqList: [21, 198, 65]);
 
   // Shorthand syntactic implementation of testIQ
-  bool checkWisdom(int IQ) => IQ > 100;
+  var checkWisdom = (int IQ) => IQ > 100;
   int randomIQ = 121;
-  print('\nPerson with IQ of $randomIQ is wise: ${checkWisdom(randomIQ)}');
+  print('\nPerson with IQ of $randomIQ is wise: ${checkWisdom(randomIQ)}\n');
+
+  // Functions are first class objects 
+  // They can be passed as parameter to another function
+  // They can also be assigned to a variable [refer Shorthand implementation]
+  List<num> list = [1, 2, 3, 4];
+  list.forEach((element) {multiply2(element);});  // Anonymous(Unnamed) Function used
+  
 
 }
+
